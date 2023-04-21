206094146e6a443971dafc75deb2c94d608447bd
Ali Alabbas <ali.n.alabbas@gmail.com>
2023-04-20 20:29:47 -0400

    ansiblels handler, handle nil case

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -385,6 +385,9 @@ require("lspconfig").nil_ls.setup {
 
 -- Simple handler to prefix ansible file location with file scheme which what neovim client expect
 local function handler(err, result, ctx, config)
+    if result == nil then
+        return
+    end
     result[1].targetUri = "file://" .. result[1].targetUri
 
     return vim.lsp.handlers['textDocument/definition'](err, result, ctx, config)
