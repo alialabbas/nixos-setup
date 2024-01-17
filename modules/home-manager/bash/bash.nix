0b4f64c77df0d428957f08aeaf1add9ca3f184d0
Ali Alabbas <ali.n.alabbas@gmail.com>
2024-01-16 19:13:24 -0500

    fix(wsl): BROWSER is not set as a session var through home-manager

--- modules/home-manager/bash/bash.nix
+++ modules/home-manager/bash/bash.nix
@@ -5,12 +5,7 @@
     enable = lib.mkDefault true;
     shellOptions = [ ];
     historyControl = [ "ignoredups" "ignorespace" ];
-    initExtra = ''
-      # Use the windows host browser if we are running in a wsl env
-      if [ ! -z "$${WSLENV}" ]; then
-          export BROWSER=wslview
-      fi
-    '';
+
     # For non-nixos system, load nix when a shell starts
     profileExtra = ''
       if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi # for non nixos env we need to load nix
--- modules/home-manager/wsl.nix
+++ modules/home-manager/wsl.nix
@@ -5,6 +5,10 @@
     ./common.nix
   ];
 
+  home.sessionVariables = {
+    BROWSER = "${pkgs.wslu}/bin/wslview";
+  };
+
   home.packages = [
     pkgs.wslu
   ];
